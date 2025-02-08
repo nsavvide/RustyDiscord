79ab51605a27034ab7cfd61fd7c7943472437fe7
nsavvide <niels.i.savvides@gmail.com>
2025-02-08 20:20:31 +0100

    :closed_lock_with_key: Add new key presses

--- src/tui/input.rs
+++ src/tui/input.rs
@@ -1,10 +1,12 @@
-use crossterm::event::{self, Event, KeyCode};
 use crate::tui::app::App;
+use crossterm::event::{self, Event, KeyCode};
 
 pub fn handle_input(app: &mut App) -> bool {
     if let Event::Key(key) = event::read().unwrap() {
         match key.code {
             KeyCode::Char('q') => return true, // Quit
+            KeyCode::Char('1') => app.selected_tab = 0,
+            KeyCode::Char('2') => app.selected_tab = 1,
             KeyCode::Tab => app.next_tab(),
             KeyCode::BackTab => app.previous_tab(),
             KeyCode::Down => app.next_channel(),
